6256c427409d6f0620386f626d5b1f85fea9c273
Marcell Mihály <mihalymarcell86@gmail.com>
2022-06-30 08:18:39 +0100

    dark mode

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,30 @@
 import moonIcon from "../assets/icon-moon.svg";
+import sunIcon from "../assets/icon-sun.svg";
 
-export default function Header() {
+export default function Header({ colorScheme, changeColorScheme }) {
   return (
     <header className="header">
       <h1 className="devfinder-logo">devfinder</h1>
-      <div className="light-switch">
-        <span>Dark</span>
-        <object type="image/svg+xml" data={moonIcon} aria-label="dark mode" />
-      </div>
-      <div hidden>
-        {/* Light <img src="../assets/icon-sun.svg" alt="" /> */}
+      <div className="light-switch" onClick={changeColorScheme}>
+        {colorScheme === "light" ? (
+          <>
+            <span>Dark</span>
+            <object
+              type="image/svg+xml"
+              data={moonIcon}
+              aria-label="switch to dark mode"
+            />
+          </>
+        ) : (
+          <>
+            <span>Light</span>
+            <object
+              type="image/svg+xml"
+              data={sunIcon}
+              aria-label="switch to light mode"
+            />
+          </>
+        )}
       </div>
     </header>
   );
